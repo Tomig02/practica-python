662f1fe64f17fe8efd3c307973af3494d687000c
Tomas Guardo <guardotomas02@gmail.com>
2021-03-24 18:19:01 -0300

    agregar hangman

--- run.py
+++
@@ -1,8 +0,0 @@
-try:
-    mensaje = int(input("Ingresa un numero: "))
-    if mensaje % 2 == 0:
-        print("este numero es par")
-    else:
-        print("este numero es inpar")
-except ValueError:
-    print("Esto no es un numero!")
