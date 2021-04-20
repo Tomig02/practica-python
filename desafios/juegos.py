5a660fb5b72cefd62de8ee402fb607b74919d287
Tomas Guardo <39739544+Tomig02@users.noreply.github.com>
2021-04-20 12:13:24 -0300

    cambios

--- desafios/juegos.py
+++ desafios/juegos.py
@@ -8,12 +8,10 @@ with open("./appstore_games.csv") as juegos:
         if 'ES' in row['Languages']:
             print(row['Name'])
     
-    maximos = []
-    for i in range(-10, 0):
-        maximos = (sorted(archivo, key = lambda archivo: archivo['User Rating Count'], reverse= True)[:10])
+    maximos = (sorted(archivo, key = lambda archivo: archivo['User Rating Count'], reverse= True)[:10])
     
-    for i in range(0, len(maximos)):
-        icon_url = maximos[i]['Icon URL']
+    for i, item in enumerate(maximos):
+        icon_url = item['Icon URL']
         icono = requests.get(icon_url)
         with open(f'./imagenes/{i}.jpg', 'wb') as f:
             f.write(icono.content)
